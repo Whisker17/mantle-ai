9cdc99615927bdb8bda577b10032e7f22438ccab
Whisker <demiwhisker@gmail.com>
2026-02-28 00:52:17 +0800

    docs: add nextra cli docs site and move plans

---
+++ docs/pages/cli/_meta.js
@@ -0,0 +1,23 @@
+export default {
+  index: {
+    title: 'Overview'
+  },
+  'getting-started': {
+    title: 'Getting Started'
+  },
+  configuration: {
+    title: 'Configuration'
+  },
+  'output-contract': {
+    title: 'Output Contract'
+  },
+  reference: {
+    title: 'Reference'
+  },
+  roadmap: {
+    title: 'Roadmap'
+  },
+  changelog: {
+    title: 'Changelog'
+  }
+}
---
+++ docs/pages/cli/reference/_meta.js
@@ -0,0 +1,17 @@
+export default {
+  'chain-balance-tx': {
+    title: 'Chain / Balance / Tx'
+  },
+  'contract-token': {
+    title: 'Contract / Token'
+  },
+  'transfer-swap': {
+    title: 'Transfer / Swap'
+  },
+  'lend-stake-yield-bridge': {
+    title: 'Lend / Stake / Yield / Bridge'
+  },
+  'providers-schema-version': {
+    title: 'Providers / Schema / Version'
+  }
+}
